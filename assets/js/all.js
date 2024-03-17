cbf52e0095e92dbb75edb617769e9dec8f09b14b
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-03-17 06:35:17 +0300

    waves full end

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,1 +1,28 @@
 "use stric"
+
+let popupOpen = document.querySelector('#popup-menu-open');
+let popupMenu = document.querySelector('.popup--wrap');
+let popupClose = document.querySelector("#popup-meny-btn");
+let popupMenuWrapper = document.querySelector('.popup--wrap');
+let video = document.querySelector("#video")
+
+
+popupOpen.addEventListener('click',()=>{
+    popupMenu.classList.toggle("popup")
+})
+
+popupClose.addEventListener('click',()=>{
+    popupMenu.classList.toggle("popup")
+    var iframeSrc = video.src;
+         video.src = iframeSrc;
+})
+
+popupMenuWrapper.addEventListener('click' , (e)=>{
+    if(e.target.classList.contains('popup--wrap')){
+        popupMenu.classList.toggle("popup")
+        // video.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+        var iframeSrc = video.src;
+            video.src = iframeSrc;
+
+    }
+})
